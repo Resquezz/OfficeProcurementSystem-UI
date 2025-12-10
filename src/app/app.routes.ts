54ce904f68c2a0503cc86d1f01f1c16301431f32
Resquezz <108730015+Resquezz@users.noreply.github.com>
2025-12-10 15:40:23 +0200

    added suppliers tab

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { BudgetsPageComponent } from './features/budgets/budgets-page.component';
 import { UsersPageComponent } from './features/users/users-page.component';
 import { CategoriesPageComponent } from './features/categories/categories-page.component';
+import { SuppliersPageComponent } from './features/suppliers/suppliers-page.component';
 
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', redirectTo: 'budgets' },
 	{ path: 'budgets', component: BudgetsPageComponent },
 	{ path: 'users', component: UsersPageComponent },
-	{ path: 'categories', component: CategoriesPageComponent }
+	{ path: 'categories', component: CategoriesPageComponent },
+	{ path: 'suppliers', component: SuppliersPageComponent }
 ];
